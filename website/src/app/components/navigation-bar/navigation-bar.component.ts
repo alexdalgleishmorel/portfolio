5bad4e73463694ceabf03930a6d71347c57f7480
alexdalgleishmorel <alex.dalgleishmorel@gmail.com>
2023-11-10 18:25:46 -0700

    made structure mobile friendly

--- website/src/app/components/navigation-bar/navigation-bar.component.ts
+++ website/src/app/components/navigation-bar/navigation-bar.component.ts
@@ -7,8 +7,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavigationBarComponent implements OnInit {
 
+  public navOptions: NavOption[] = [
+    NavOption.ABOUT, NavOption.EXPERIENCE, NavOption.PROJECTS
+  ];
+  public selectedNavOption: NavOption = NavOption.ABOUT;
+
   constructor() { }
 
   ngOnInit() {}
 
+  public getSelectedNavOption(): NavOption {
+    return this.selectedNavOption;
+  }
+
+  public setSelectedNavOption(option: NavOption) {
+    this.selectedNavOption = option;
+  }
+
+  public isNavOptionSelected(option: NavOption): boolean {
+    return this.selectedNavOption === option;
+  }
+}
+
+export enum NavOption {
+  ABOUT = 'ABOUT',
+  EXPERIENCE = 'EXPERIENCE',
+  PROJECTS = 'PROJECTS'
 }
