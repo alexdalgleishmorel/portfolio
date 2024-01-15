1ef58589d21cfcff1eaf638c10191dd277956b53
alexdalgleishmorel <alex.dalgleishmorel@gmail.com>
2024-01-15 16:22:49 -0700

    outline for about section

--- website/src/app/components/content/content.component.ts
+++ website/src/app/components/content/content.component.ts
@@ -7,7 +7,7 @@ import { Component } from '@angular/core';
 })
 export class ContentComponent {
   
-  public selectedNavOption: NavOption = NavOption.PROJECTS;
+  public selectedNavOption: NavOption = NavOption.ABOUT;
 
   constructor() {}
 
