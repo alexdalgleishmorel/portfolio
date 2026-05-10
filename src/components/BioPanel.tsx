821020bd1b3b2c67952f66eedb1fdd69c16dbba7
alexdalgleishmorel <alex.dalgleishmorel@gmail.com>
2026-05-09 23:35:02 -0600

    Update bio description

--- src/components/BioPanel.tsx
+++ src/components/BioPanel.tsx
@@ -1,5 +1,7 @@
 export const BioPanel = () => (
   <div className="panel glass bio-panel">
-    <div className="bio-text">Designer and developer crafting thoughtful digital experiences.</div>
+    <div className="bio-text">
+      Full-stack developer drawn to data visualization. I shine where system-design problems meet backend optimization.
+    </div>
   </div>
 );
